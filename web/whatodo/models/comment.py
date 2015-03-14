0e071adf9cf45d4c0d2ff523bc40dd9f0406003c
Salma El Alaoui Talibi <s.elalaouitalibi@gmail.com>
2015-03-14 12:55:41 +0100

    added self function to event

--- web/whatodo/models/comment.py
+++ web/whatodo/models/comment.py
@@ -5,3 +5,4 @@ from .event import Event
 class Comment(models.Model):
 	user = models.ForeignKey(User)
 	event = models.ForeignKey(Event)
+	
--- web/whatodo/models/event.py
+++ web/whatodo/models/event.py
@@ -6,25 +6,26 @@ from .category import Category
 
 class Event(models.Model):
 
-    name = models.CharField(max_length = 30)
-    description = models.CharField (max_length = 200)
-    url = models.URLField()
-    #TODO: image or Video field, do we really require a model for that?
-    startTime = models.TimeField(auto_now = False, auto_now_add = False)
-    endTime = models.TimeField(auto_now = False, auto_now_add = False)
-    startDate = models.DateField(auto_now = False, auto_now_add = False) 
-    endDate = models.DateField(auto_now = False, auto_now_add = False) 
-    price = models.DecimalField(max_digits = 5, decimal_places = 2)
-    min_age = models.IntegerField()
-    
+	name = models.CharField(max_length = 30)
+	description = models.CharField (max_length = 200)
+	url = models.URLField()
+	#TODO: image or Video field, do we really require a model for that?
+	startTime = models.TimeField(auto_now = False, auto_now_add = False)
+	endTime = models.TimeField(auto_now = False, auto_now_add = False)
+	startDate = models.DateField(auto_now = False, auto_now_add = False) 
+	endDate = models.DateField(auto_now = False, auto_now_add = False) 
+	price = models.DecimalField(max_digits = 5, decimal_places = 2)
+	min_age = models.IntegerField()
+	
 
-    addresses = models.ManyToManyField(Address)
-    categories = models.ManyToManyField(Category)
-    #organizer = models.ForeignKey(User) 
-    tags = models.ManyToManyField(Tag)
+	addresses = models.ManyToManyField(Address)
+	categories = models.ManyToManyField(Category)
+	#organizer = models.ForeignKey(User) 
+	tags = models.ManyToManyField(Tag)
 
+	def __str__(self):
+		return self.name
 
-    
 
 
 
