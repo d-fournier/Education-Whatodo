3470632eefa66c789f8272fc85e38457e7a50b19
Salma El Alaoui Talibi <s.elalaouitalibi@gmail.com>
2015-03-16 18:12:44 +0100

    added django-rest

--- web/whatodo/urls.py
+++ web/whatodo/urls.py
@@ -6,6 +6,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'web.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^$', 'whatodo.views.home', name='home'),
+    url(r'^$', 'whatodo.views.home', name='home'),   
 )
 
