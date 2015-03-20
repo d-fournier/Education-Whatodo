7bc3060c96be52861f094f44d977f6ee252a6e04
Zied Thabet <zied.thabet1@gmail.com>
2015-03-20 15:28:36 +0100

    Address & City models

--- web/whatodo/models/city.py
+++ web/whatodo/models/city.py
@@ -1,4 +1,11 @@
 from django.db import models
 
 class City(models.Model):
-	pass
+	id = models.AutoField(primary_key=True)
+	
+	name = models.CharField(max_length = 50)
+	ZIPcode = models.CharField(max_length = 10)
+
+	def __str__(self):
+		return self.name + " (" + self.ZIPcode + ")"
+
