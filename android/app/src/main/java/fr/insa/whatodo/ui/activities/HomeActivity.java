62aa1cd8566d2d41ed421d7199de33c536234b1f
Benjamin Legrand <benjamin.legrand93@gmail.com>
2015-03-24 16:11:40 +0100

    Com avec le serveur + erreur si pas de connexion

--- android/app/src/main/java/fr/insa/whatodo/ui/activities/HomeActivity.java
+++ android/app/src/main/java/fr/insa/whatodo/ui/activities/HomeActivity.java
@@ -1,26 +1,34 @@
 package fr.insa.whatodo.ui.activities;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.SearchView;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import fr.insa.whatodo.R;
 import fr.insa.whatodo.models.Event;
-import fr.insa.whatodo.services.GetEventsTask;
 import fr.insa.whatodo.ui.fragments.CustomMapFragment;
+import fr.insa.whatodo.ui.fragments.DownloadFailedFragment;
 import fr.insa.whatodo.ui.fragments.EventListFragment;
 import fr.insa.whatodo.ui.fragments.NavigationDrawerFragment;
 import fr.insa.whatodo.utils.OnListChangedListener;
@@ -43,6 +51,7 @@ public class HomeActivity extends ActionBarActivity
     private SearchView searchBar;
     private EventListFragment eventListFragment;
     private CustomMapFragment mapFragment;
+    private DownloadFailedFragment downloadFragment;
     private ArrayList<Event> eventList;
     private List<OnListChangedListener> mListeners;
     private List<Event> mDisplayedEvents;
@@ -57,10 +66,11 @@ public class HomeActivity extends ActionBarActivity
         mListeners = new ArrayList<>();
         eventListFragment = EventListFragment.newInstance(eventList);
         mapFragment = CustomMapFragment.newInstance(eventList);
+        downloadFragment = new DownloadFailedFragment();
 
         // Add the fragment to the 'fragment_container' FrameLayout
         getSupportFragmentManager().beginTransaction()
-                .add(R.id.fragment_home_container, eventListFragment).commit();
+                .add(R.id.fragment_home_container, downloadFragment).commit();
         searchBar = (SearchView) findViewById(R.id.home_search_bar);
 
         searchBar.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
@@ -97,7 +107,7 @@ public class HomeActivity extends ActionBarActivity
         FragmentManager fragmentManager = getSupportFragmentManager();
         switch (position) {
             case (0):
-            //    fragmentManager.beginTransaction().replace(R.id.fragment_home_container, eventListFragment).commit();
+                //    fragmentManager.beginTransaction().replace(R.id.fragment_home_container, eventListFragment).commit();
                 break;
             case (1):
 
@@ -168,6 +178,9 @@ public class HomeActivity extends ActionBarActivity
             case (R.id.action_list):
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_home_container, eventListFragment).commit();
                 break;
+            case (R.id.action_refresh) :
+                new GetEventsTask().execute("http://dfournier.ovh/api/event/?format=json", null, "");
+                break;
 
         }
 
@@ -176,18 +189,16 @@ public class HomeActivity extends ActionBarActivity
 
     public void fillEventList() {
         eventList = new ArrayList<>();
-        String response = null;
 
-        eventList.add(new Event(getResources().getDrawable(R.drawable.ic_launcher), new Date(), null, "Evt 1", "10 euros", "19 Rue Marcel Dutarte 69100 Villeurbanne", "C'est cool venez"));
+/*      eventList.add(new Event(getResources().getDrawable(R.drawable.ic_launcher), new Date(), null, "Evt 1", "10 euros", "19 Rue Marcel Dutarte 69100 Villeurbanne", "C'est cool venez"));
         eventList.add(new Event(getResources().getDrawable(R.drawable.ic_launcher), new Date(), new Date(115,05,21), "Evenement 2", "10 euros", "3 Rue du Château d'Eau 70100 Beaujeu", "C'est cool venez"));
         eventList.add(new Event(getResources().getDrawable(R.drawable.ic_launcher), new Date(), null, "Evt 3", "10 euros", "820 S Michigan Ave Chicago IL 60605-7102", "C'est cool venez"));
         eventList.add(new Event(getResources().getDrawable(R.drawable.ic_launcher), new Date(), new Date(115, 06, 31), "Evenement 4", "10 euros", "69, rue Farabi Marrakech Maroc", "C'est cool venez"));
-
-        new GetEventsTask().execute("http://dfournier.ovh", null, response);
+*/
     }
 
     private void notifyListChanged() {
-        for(OnListChangedListener list : mListeners){
+        for (OnListChangedListener list : mListeners) {
             list.onListChanged(mDisplayedEvents);
         }
     }
@@ -198,5 +209,97 @@ public class HomeActivity extends ActionBarActivity
 
     public void removeOnListChangedListener(OnListChangedListener list) {
         mListeners.remove(list);
+    }
+
+    public class GetEventsTask extends AsyncTask<String, Void, String> {
+
+        ProgressDialog dialog = null;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            ConnectivityManager connMgr = (ConnectivityManager)
+                    getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo networkInfo = connMgr.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+            boolean isWifiConn = networkInfo.isConnected();
+            networkInfo = connMgr.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+            boolean isMobileConn = networkInfo.isConnected();
+            if (isWifiConn || isMobileConn) {
+                //On ne fait rien, on est bien connecté à internet
+                dialog = ProgressDialog.show(HomeActivity.this, null, getString(R.string.download));
+            } else {
+                //Pas de connexion internet
+                HomeActivity.this.getSupportFragmentManager().beginTransaction().replace(R.id.fragment_home_container, downloadFragment).commit();
+                this.cancel(true);
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            dialog.dismiss();
+            dialog = ProgressDialog.show(HomeActivity.this, null, getString(R.string.parse));
+            //TODO Il faut parser la string ici !
+            dialog.dismiss();
+            HomeActivity.this.getSupportFragmentManager().beginTransaction().replace(R.id.fragment_home_container, eventListFragment).commit();
+        }
+
+        protected String doInBackground(String... urls) {
+            // These two need to be declared outside the try/catch
+            // so that they can be closed in the finally block.
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            // Will contain the raw JSON response as a string.
+            String eventJsonStr = null;
+
+            try {
+                // Construct the URL for the server query
+                URL url = new URL(urls[0]);
+
+                // Create the request to the server, and open the connection
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    return null;
+                }
+                eventJsonStr = buffer.toString();
+            } catch (IOException e) {
+                return "network_error";
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e("Getting Events", "Error closing stream", e);
+                    }
+                }
+            }
+            return eventJsonStr;
+        }
+
     }
 }
