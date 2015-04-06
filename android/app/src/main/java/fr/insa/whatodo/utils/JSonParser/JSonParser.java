c3b2b0cd6fcb33d5f2e33645e209d1af9711b66b
William Vitali <ebill@hotmail.fr>
2015-04-06 22:14:26 +0200

    City Parser

--- android/app/src/main/java/fr/insa/whatodo/utils/JSonParser/JSonParser.java
+++ android/app/src/main/java/fr/insa/whatodo/utils/JSonParser/JSonParser.java
@@ -10,6 +10,7 @@ import java.io.Reader;
 import java.util.ArrayList;
 
 import fr.insa.whatodo.models.Category;
+import fr.insa.whatodo.models.City;
 import fr.insa.whatodo.models.Event;
 import fr.insa.whatodo.models.Tag;
 
@@ -61,6 +62,21 @@ public class JSonParser{
         JSonTagsAnswer response = gson.fromJson(reader, JSonTagsAnswer.class);
 
         System.out.println("Il y a "+response.getCount()+" tags !");
+
+        return response.getResults();
+
+    }
+
+    public ArrayList<City> parseCities(InputStream source) {
+
+
+        Gson gson = new Gson();
+
+        Reader reader = new InputStreamReader(source);
+
+        JSonCitiesAnswer response = gson.fromJson(reader, JSonCitiesAnswer.class);
+
+        System.out.println("Il y a "+response.getCount()+" villes !");
 
         return response.getResults();
 
