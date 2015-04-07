491c525d8f9349f1d78745cc8ec7bb969cb82dde
Donovan <donof43@gmail.com>
2015-04-07 18:52:02 +0200

    Fix compile errors

--- web/whatodo/views.py
+++ web/whatodo/views.py
@@ -10,7 +10,6 @@ from .models.event import Event
 from .models.city import City
 from .filters import EventFilter
 
-
 class EventViewSet(viewsets.ModelViewSet):
 	queryset = Event.objects.all()
 	filter_class = EventFilter
@@ -20,18 +19,27 @@ class EventViewSet(viewsets.ModelViewSet):
 		return EventCreateSerializer
 
 	def get_queryset(self):
-        legal_age_param = self.request.QUERY_PARAMS.get('legal_age', None)
-        city_param = self.request.QUERY_PARAMS.get('city', None)
-        distance_param = self.request.QUERY_PARAMS.get('distance', None)
-        if legal_age_param is not None:
+		queryset = Event.objects.all()
+		legal_age_param = self.request.QUERY_PARAMS.get('legal_age', None)
+		city_param = self.request.QUERY_PARAMS.get('city', None)
+		distance_param = self.request.QUERY_PARAMS.get('distance', None)
+		if legal_age_param is not None:
 			if legal_age_param:
 				queryset = queryset.filter(min_age__gte=18)
 			else:
 				queryset = queryset.filter(min_age__lt=18)
-        if city_param is not None and distance_param is not None:
-				City.objects.
-				"""'city', 'distance',"""
-        return queryset
+		if city_param is not None and distance_param is not None:
+			try:
+				city = City.objects.get(id=city_param)
+			except app.DoesNotExist:
+				pass
+			lat_dist = change_in_latitude_km(distance_param)
+			long_dist = change_in_longitude_km(lat_dist, distance_param)
+			queryset = queryset.filter(latitude__lte=(city.latitude + lat_dist))
+			queryset = queryset.filter(latitude__gte=(city.latitude - lat_dist ))
+			queryset = queryset.filter(longitude__gte=(city.longitude - long_dist))
+			queryset = queryset.filter(longitude__lte=(city.longitude + long_dist))
+		return queryset
 
 	
 class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
