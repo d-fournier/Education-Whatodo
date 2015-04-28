6451370c3e460bc5d2920b6585293b4595ee9c3d
Donovan <donof43@gmail.com>
2015-04-28 17:06:56 +0200

    Fix

--- web/web/settings.py
+++ web/web/settings.py
@@ -103,6 +103,7 @@ REST_FRAMEWORK = {
 	# or allow read-only access for unauthenticated users.
 	'DEFAULT_AUTHENTICATION_CLASSES': (
 		'rest_framework.authentication.TokenAuthentication',
+		'rest_framework.authentication.SessionAuthentication',
 	),
 	'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAuthenticatedOrReadOnly',),
 	'PAGE_SIZE' : 100,
