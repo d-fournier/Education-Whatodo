bce523f8b33cd964d50c8eb904cb9150561f6ef7
Fournier Donovan <donof43@gmail.com>
2015-05-02 01:59:47 +0200

    Fix Bug

--- web/web/settings.py
+++ web/web/settings.py
@@ -66,7 +66,7 @@ CORS_ORIGIN_WHITELIST = (
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
-ATABASES = {
+DATABASES = {
 	'default': {
 		'ENGINE': 'django.db.backends.sqlite3',
 		'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
